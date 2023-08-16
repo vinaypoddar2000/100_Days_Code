2cc1632f8f146127b50539320f2670093f366fac
vinaypoddar2000 <73072291+vinaypoddar402@users.noreply.github.com>
2023-08-17 00:39:04 +0530

    Day One

---
+++ Day_1/Day_1.cpp
@@ -0,0 +1,15 @@
+double areaSwitchCase(int ch, vector<double> a) {
+	// Write your code here
+	double area=0.0;
+	switch(ch){
+		case 1:
+		    area = M_PI*a[0]*a[0];
+			break;
+		case 2: 
+		    area = a[0]*a[1];
+			
+		
+	}
+	return area;
+	
+}
