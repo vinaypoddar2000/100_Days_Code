e98fae1cc2d407f6d8e806601634441961558b7c
vinaypoddar2000 <73072291+vinaypoddar402@users.noreply.github.com>
2023-08-27 22:41:01 +0530

    Day_10

---
+++ Day_10/pattern1.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    for(int i=n;i>=1;i--){
+	        for(int j=1;j<=i;j++){
+	            cout<<"* ";
+	        }
+	        cout<<endl;
+	    }
+	}
+};
