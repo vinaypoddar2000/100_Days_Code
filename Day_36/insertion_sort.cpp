63c2cce28e0ea10adf501b0143b6ad62d38ac082
vinaypoddar2000 <73072291+vinaypoddar402@users.noreply.github.com>
2023-09-22 00:06:03 +0530

    Day_36

---
+++ Day_36/insertion_sort.cpp
@@ -0,0 +1,13 @@
+void insertionSort(vector<int>&arr, int n)
+{
+    //write your code here
+    for(int i=0;i<=n-1;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
