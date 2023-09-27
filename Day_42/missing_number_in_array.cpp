be9b4923f7a38b4ba86f7a41911e21e0a299cca8
vinaypoddar2000 <73072291+vinaypoddar402@users.noreply.github.com>
2023-09-27 23:39:29 +0530

    Day_42

---
+++ Day_42/missing_number_in_array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sums = (n*(n+1))/2;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        return sums-sum;
+        
+        
+    }
+};
